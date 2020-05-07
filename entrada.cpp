bf7305e8ffda48120315fce98b53664e2fb6ba6e
zaza <contact@zaza.com>
2020-05-06 23:07:18 -0500

    entrada de datos

---
+++ entrada.cpp
@@ -0,0 +1,16 @@
+// lectura o entrada de datos
+
+#include<iostream>
+
+using namespace	std;
+
+int main(){
+	int numero;
+	
+	cout<<"Digite un numero: ";
+	cin>>numero;
+	
+	cout<<"\nEl numero que  digito es: "<<numero;
+	
+	return 0;
+}
