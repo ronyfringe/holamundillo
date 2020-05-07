4619be43874a43c015824375cb9691f0ca6e11ea
zaza <contact@zaza.com>
2020-05-06 23:10:35 -0500

    commit de entrada de datos

--- entrada.cpp
+++ entrada.cpp
@@ -7,8 +7,8 @@ using namespace	std;
 int main(){
 	int numero;
 	
-	cout<<"Digite un numero: ";
-	cin>>numero;
+	cout<<"Digite un numero: "; 
+	cin>>numero; // usando cin
 	
 	cout<<"\nEl numero que  digito es: "<<numero;
 	
